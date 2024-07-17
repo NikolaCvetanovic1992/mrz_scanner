23b6280216949409ae9dbe8378525f45183c98ab
Nikola Cvetanovic <nikola.cvetanovic@inkaviaton.com>
2024-07-17 14:44:25 +0200

    added mrz parser

--- lib/mrz_scanner.dart
+++ lib/mrz_scanner.dart
@@ -2,5 +2,5 @@ library mrz_scanner;
 
 export 'src/mrz_scanner.dart';
 export 'src/mrz_controller.dart';
-export 'src/mrz_parser/mrz_parser/lib/mrz_parser.dart';
+export 'src/mrz_parser_lib/lib/mrz_parser.dart';
 export 'package:camera_platform_interface/src/types/camera_description.dart';
